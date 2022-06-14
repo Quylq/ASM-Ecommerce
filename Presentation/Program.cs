f7afe4181046059dbf245ef449ef2c46530969c3
Quylq <quylq.114010121002@vtc.edu.vn>
2022-06-14 23:46:19 +0700

    Project1.2

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -4,27 +4,59 @@ using Persistence;
 Console.OutputEncoding = System.Text.Encoding.Unicode;
 Console.InputEncoding = System.Text.Encoding.Unicode;
 
-Console.Write("Nhập Tên Đăng Nhập: ");
-string acc_name = Console.ReadLine();
-Console.Write("Nhập Mật Khẩu: ");
-string acc_pass = Console.ReadLine();
-
-AccountBL accountBL = new AccountBL();
-
-Account acc =  accountBL.GetAccountByName(acc_name);
-
-if (acc != null)
+Console.WriteLine("1. Đăng Nhập: ");
+Console.WriteLine("2. Đăng Ký: ");
+Console.Write("Chọn: ");
+int choice = Convert.ToInt32(Console.ReadLine());
+switch (choice)
 {
-    if (acc_pass == acc.AccountPassword)
+    case 1: 
+        Login();
+        break;
+    case 2: 
+        SigUp();
+        break;
+    default:
+        break;
+}
+
+
+static void Login()
+{
+    Console.Write("Nhập Tên Đăng Nhập: ");
+    string acc_name = Console.ReadLine();
+    Console.Write("Nhập Mật Khẩu: ");
+    string acc_pass = Console.ReadLine();
+
+    AccountBL accountBL = new AccountBL();
+    Account acc =  accountBL.GetAccountByName(acc_name);
+
+    if (acc != null)
     {
-        Console.WriteLine($"Đăng nhập thành công");
+        if (acc_pass == acc.AccountPassword)
+        {
+            Console.WriteLine($"Đăng nhập thành công");
+        }
+        else
+        {
+            Console.WriteLine($"Sai mật khẩu");
+        }
     }
     else
     {
-        Console.WriteLine($"Sai mật khẩu");
+        Console.WriteLine($"Tài khoản không tồn tại");
     }
 }
-else
+
+static void SigUp()
 {
-    Console.WriteLine($"Tài khoản không tồn tại");
+    Console.Write("Nhập Tên Đăng Nhập: ");
+    string acc_name = Console.ReadLine();
+    Console.Write("Nhập Mật Khẩu: ");
+    string acc_pass = Console.ReadLine();
+    Console.Write("Nhập Vai Trò: ");
+    string acc_role = Console.ReadLine();
+
+    AccountBL accountBL = new AccountBL();
+    accountBL.SaveAccount(acc_name, acc_pass, acc_role);
 }
